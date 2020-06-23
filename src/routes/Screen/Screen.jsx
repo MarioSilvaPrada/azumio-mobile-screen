9eb08956f32c8451c60e99b2ac3659eb1f68b966
Mário Prada <mariosilvaprada@gmail.com>
2020-06-24 00:00:48 +0100

    build

--- src/routes/Screen/Screen.jsx
+++ src/routes/Screen/Screen.jsx
@@ -7,42 +7,37 @@ import Option from 'components/Option';
 const Screen = () => {
   const optionsData = [
     {
-      value: 'Blood Glucose',
-      action: () => console.log('Blood Glucose selected'),
+      value: 'Blood Glucose'
     },
     {
-      value: 'Medication',
-      action: () => console.log('Medication selected'),
+      value: 'Medication'
     },
     {
-      value: 'Food Tracking',
-      action: () => console.log('Food Tracking selected'),
+      value: 'Food Tracking'
     },
     {
-      value: 'Exercise',
-      action: () => console.log('Exercise selected'),
+      value: 'Exercise'
     },
     {
-      value: 'Weight Management',
-      action: () => console.log('Weight Management selected'),
+      value: 'Weight Management'
     },
     {
-      value: 'Blood Pressure',
-      action: () => console.log('Blood Pressure selected'),
+      value: 'Blood Pressure'
     },
     {
-      value: 'A1C',
-      action: () => console.log('A1C selected'),
-    },
+      value: 'A1C'
+    }
   ];
   return (
     <div className="container">
-      <div className="header">
-        <h1 className="header-title">What are your main areas of focus?</h1>
-      </div>
-      <div className="description">This will help us build a personalized experience for you.</div>
-      <div className="options-wrapper">
-        {optionsData.map(({ value }) => <Option key={value} value={value} />)}
+      <div>
+        <div className="header">
+          <h1 className="header-title">What are your main areas of focus?</h1>
+        </div>
+        <div className="description">This will help us build a personalized experience for you.</div>
+        <div className="options-wrapper">
+          {optionsData.map(({ value }) => <Option key={value} value={value} />)}
+        </div>
       </div>
       <div className="footer">
         <div className="footer-btn__goBack">Back</div>
@@ -53,7 +48,7 @@ const Screen = () => {
           <div className="dot" />
           <div className="dot current" />
         </div>
-        <div className="footer-btn__done">Done</div>
+        <div className="footer-btn__done btn-main">Done</div>
       </div>
     </div>
   );
