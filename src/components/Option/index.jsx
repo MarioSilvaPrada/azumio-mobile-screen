20997c8c52c437a057ad7c5759e2f29b70fa5515
Mário Prada <mariosilvaprada@gmail.com>
2020-06-23 23:50:38 +0100

    updating css

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Option.css';
 
 const Option = ({ value }) => {
-  const [ isSelected, setIsSelected ] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
   return (
     <button
       type="button"
